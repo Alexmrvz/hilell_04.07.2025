f2e172b74c8eeaf8652430a4f551590d01e66780
Alexmrvz <alexmraz43@gmail.com>
2025-07-11 16:09:00 +0300

    lesson-2 commented

--- lesson1/lesson.py
+++ lesson1/lesson.py
@@ -1,5 +1,8 @@
-# print("Hello world!")
-# print("End world!")
-# print("Start world!")
-# print("Stop world!")
-# print("hello Evgeniy")
+number = (input("Enter a 4-digit number"))
+if len(number) != 4:
+    print("Error: enter 4 digit number")
+else:
+    print(number[0])
+    print(number[1])
+    print(number[2])
+    print(number[3])
