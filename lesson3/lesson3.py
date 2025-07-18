230d1cae31aac824ec7f5d2f3590ddded29f3ff4
Alexmrvz <alexmraz43@gmail.com>
2025-07-18 21:45:24 +0300

    lesson-3

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -1,24 +1,23 @@
-def calculator(num1, num2, operation):
-    try:
-        result = ""
-        if operation == "+":
-            result = num1+num2
-        if operation == "-":
-            result = num1-num2
-        if operation == "*":
-            result = num1*num2
-        if operation == "/":
-            if num2 == 0 or num2 == 0:
-                print("dividing to zero is impossible!")
-            else:
-                result = num1/num2
-        print(result)
-        return result
-    except ValueError:
-        print("Enter a valid number")
+try:
+    number1 = float(input("Enter the first number: "))
+    number2 = float(input("Enter the second number: "))
+    operator = input("Enter the operation (+, -, *, /): ")
+    result = ""
+    if operator == "+":
+        result = number1 + number2
+    elif operator == "-":
+        result = number1 - number2
+    elif operator == "*":
+        result = number1 * number2
+    elif operator == "/":
+        if number2 == 0:
+            print("Dividing by zero is impossible!")
+        else:
+            result = number1 / number2
+    else:
+        print("Invalid operation!")
 
-number1 = float(input("Enter the first number: "))
-number2 = float(input("Enter the second number: "))
-operator = input("Enter the operation: ")
-calculator(number1, number2, operator)
-
+    if result != "":
+        print("Result:", result)
+except ValueError:
+    print("Enter a valid number")
--- lesson3/lesson3_2.py
+++ lesson3/lesson3_2.py
@@ -1,8 +1,9 @@
-def reverse(lst):
-    if len(lst) == 0:
-        return []
-    lst.insert(0, lst.pop())
-    return lst
-
 array_of_numbers = [12, 3, 4, 10]
-print(reverse(array_of_numbers))
+
+if len(array_of_numbers) == 0:
+    reversed_list = []
+else:
+    array_of_numbers.insert(0, array_of_numbers.pop())
+    reversed_list = array_of_numbers
+
+print(reversed_list)
