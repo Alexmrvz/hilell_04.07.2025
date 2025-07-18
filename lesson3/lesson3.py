ac7d3d523b7aec77c7aa65a636c1bd6c970f5f58
Alexmrvz <alexmraz43@gmail.com>
2025-07-18 17:22:28 +0300

    lesson-3

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -25,3 +25,4 @@ number1 = float(input("Enter the first number: "))
 number2 = float(input("Enter the second number: "))
 operator = input("Enter the operation: ")
 calculator(number1, number2, operator)
+
