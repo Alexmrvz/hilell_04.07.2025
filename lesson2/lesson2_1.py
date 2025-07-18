767a73bffb80c7ffc35da6006dddd54492afb0bf
Alexmrvz <alexmraz43@gmail.com>
2025-07-18 17:20:30 +0300

    lesson-2

--- lesson2/lesson2_1.py
+++ lesson2/lesson2_1.py
@@ -1,5 +1,6 @@
 number = (input("Enter a 4-digit number"))
-if len(number) != 4:
+if len(number) != 4
+if len(number) != 0,1,2,3,4,5,6,7,8,9:
     print("Error: enter 4 digit number")
 else:
     print(number[0])
--- lesson2/lesson2_2.py
+++ lesson2/lesson2_2.py
@@ -1,6 +1,3 @@
 number1 = input("Enter a 5 - digit number")
 reversed_number = number1[::-1]
-if len(number1) != 5:
-    print("Error: enter 5 digit number")
-else:
-    print(reversed_number)
+print(reversed_number)
