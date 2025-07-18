d0d75aebe5febb50f2577e22bc133518c3ee35fc
Alexmrvz <alexmraz43@gmail.com>
2025-07-18 17:21:44 +0300

    lesson-2

---
+++ lesson3/lesson3.py
@@ -0,0 +1,27 @@
+def calculator(num1, num2, operation):
+    try:
+        # num1 = float(input("Enter the number pls"))
+        # operation = input("Enter the operation pls")
+        # num2 = float(input("Enter the number pls"))
+        result = ""
+        if operation == "+":
+            result = num1+num2
+        if operation == "-":
+            result = num1-num2
+        if operation == "*":
+            result = num1*num2
+        if operation == "/":
+            if num2 == 0:
+                print("dividing to zero is impossible!")
+            if num1 == 0:
+                print("dividing to zero is impossible!")
+            result = num1/num2
+        print(result)
+        return result
+    except ValueError:
+        print("Enter a valid number")
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+operator = input("Enter the operation: ")
+calculator(number1, number2, operator)
