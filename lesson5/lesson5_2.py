dddc93d0580c50ce5c7370a3d203af7bb6683965
Alexmrvz <alexmraz43@gmail.com>
2025-07-25 16:44:51 +0300

    lesson-5

---
+++ lesson5/lesson5_2.py
@@ -0,0 +1,28 @@
+while True:
+    try:
+        number1 = float(input("Enter the first number: "))
+        number2 = float(input("Enter the second number: "))
+        operator = input("Enter the operation (+, -, *, /): ")
+        result = ""
+        if operator == "+":
+            result = number1 + number2
+        elif operator == "-":
+            result = number1 - number2
+        elif operator == "*":
+            result = number1 * number2
+        elif operator == "/":
+            if number2 == 0:
+                print("Dividing by zero is impossible!")
+            else:
+                result = number1 / number2
+        else:
+            print("Invalid operation!")
+
+        if result != "":
+            print("Result:", result)
+    except ValueError:
+        print("Enter a valid number")
+    again = input("Next? (yes or y to continue): ").lower()
+    if again != 'y' and again != 'yes':
+        print("finished.")
+        break
