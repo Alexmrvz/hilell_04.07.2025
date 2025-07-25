983a1a87376f1ab7b008c70070b9ef9938accebe
Alexmrvz <alexmraz43@gmail.com>
2025-07-25 16:48:35 +0300

    lesson-4

--- lesson4/lesson4_3.py
+++ lesson4/lesson4_3.py
@@ -1,6 +1,6 @@
 import random
 length = random.randint(3, 10)
-list = [random.randint(1, 10) for _ in range(length)]
-print(list)
-selected = [list[0], list[2], list[-2]]
+numbers = [random.randint(1, 10) for _ in range(length)]
+print(numbers)
+selected = [numbers[0], numbers[2], numbers[-2]]
 print(selected)
