dad28a1ec2273766976bc40f95117f298d8952ee
Alexmrvz <alexmraz43@gmail.com>
2025-07-30 17:15:45 +0300

    lesson-2

--- lesson2/lesson2_2.py
+++ lesson2/lesson2_2.py
@@ -1,11 +1,20 @@
-number = int(input("Enter a 5 - digit number"))
+# number = int(input("Enter a 5 - digit number"))
+#
+# a, number = divmod(number, 10)
+# b, number = divmod(number, 10)
+# c, number = divmod(number, 10)
+# d, number = divmod(number, 10)
+# e = number
+#
+# reversed_number = a + b * 10 + c * 100 + d * 1000 + e * 10000
 
-a, number = divmod(number, 10)
-b, number = divmod(number, 10)
-c, number = divmod(number, 10)
-d, number = divmod(number, 10)
-e = number
+# print(reversed_number)
 
-reversed_number = a + b * 10 + c * 100 + d * 1000 + e * 10000
 
+
+
+number = input("Enter a 5 - digit number")
+
+number = str(number)
+reversed_number = number[::-1]
 print(reversed_number)
