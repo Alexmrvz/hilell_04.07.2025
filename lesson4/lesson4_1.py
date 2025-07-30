0d73abad7b6218b32844b69b3f5a557e444974b6
Alexmrvz <alexmraz43@gmail.com>
2025-07-30 17:48:01 +0300

    lesson-4

--- lesson4/lesson4_1.py
+++ lesson4/lesson4_1.py
@@ -1,5 +1,8 @@
 lst = [0, 1, 0, 12, 3]
 zero_count = lst.count(0)
-non_zeros = [x for x in lst if x != 0]
-lst = non_zeros + zero_count * [0]
+while 0 in lst:
+    lst.remove(0)
+for _ in range(zero_count):
+    lst.append(0)
+
 print(lst)
